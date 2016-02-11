6f4e06bdbbc3fb4630309c7a95e6fe565b6dab5c
git config --global user.name "Link1965 <git config --global user.email nicci.hydak@gmail.com>
2016-02-10 21:18:38 -0500

    addition of checkbox

--- js/app.js
+++ js/app.js
@@ -3,8 +3,9 @@ $(document).ready(function() {
      var listvalue = $(".input-box").val();
      $(".input-box").val();
 	 $(".list-items").append('<li>' + listvalue + '</li>');
-
+	 $('<input type="checkbox" value="0" />').prependTo('li');
     });
+
 
 
 
